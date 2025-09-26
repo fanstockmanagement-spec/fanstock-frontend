ebd9d987d65e54e003ae7d5d961aae01e34b105a
pascalniri <pascalniri@gmail.com>
2025-09-26 11:20:20 +0200

    Created the app and installed required dependencies

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div>
+        <h1>250Kicks</h1>
+      </div>
+    </main>
+  );
+}
